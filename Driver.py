693506cc53abdfe5c1c478f5d7e2c8d50190ef26
Evan <johannse@hartwick.edu>
2020-03-26 23:36:43 -0400

    update

--- Driver.py
+++ Driver.py
@@ -3,23 +3,23 @@
 
 from StackandQueue import Stack, Queue
 
-one = 1
-method_choice = 0
-while method_choice != 1 or method_choice != 2:
+choice = 0
+
+while choice != 1 or choice != 2: # the loop here is broken and i am not sure how to fix it, i have spent the lst 4 hours on this loop alone trying to figure out why it wont work
     print("""Which method would you like to use?
     1. Stack(FIFO)
-    2. Queue(LIFO)""")
-    method_choice = int(input(">>"))
-    if method_choice == 1:
+    2. Queue(LIFO) """)
+    choice = int(input(">> ")) # after creating these Linked lists it then somehow resets the 'choice' variable to 0 and reruns the loop, it refuses to exit even if the choice is now 1 or 2
+    if choice == 1:
         Num_stock = Stack()
         Price = Stack()
-        print(" hi ")
-        # Num_stock.push(10)
-        # print(Num_stock)
+
+
     else:
         Num_stock = Queue()
         Price = Queue()
-        print(" hello ")
+
+
 
 total_inventory = 0
 total_profit = 0.0
@@ -32,8 +32,9 @@ while menu_choice != 5:
     1. Add stock
     2. Sell stock
     3. Check Total
-    4. Check Profit""")
-
+    4. Check Profit
+    5. Exit""")
+    menu_choice = int(input(">> "))
     if menu_choice == 1:
         qty = int(input("How much is being added?: "))
         price = int(input("How much does each share cost?: "))
